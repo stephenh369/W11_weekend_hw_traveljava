89f81999044a1c691b63db9797295dc4b58c2f0f
Stephen <stephen_h369@live.co.uk>
2020-08-28 13:39:46 +0100

    plane tests for getting type, type weight and type seats pass

--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    Plane plane1;
+    Plane plane2;
+    Plane plane3;
+
+    @Before
+    public void before() {
+        plane1 = new Plane(PlaneType.BOEING747);
+        plane2 = new Plane(PlaneType.AIRBUSA350);
+        plane3 = new Plane(PlaneType.EMPTYPLANE);
+    }
+
+    @Test
+    public void canGetType() {
+        assertEquals(PlaneType.BOEING747, plane1.getType());
+    }
+
+    @Test
+    public void canGetTotalWeight() {
+        assertEquals(450, plane1.getType().getWeight());
+    }
+
+    @Test
+    public void canGetTotalSeats() {
+        assertEquals(350, plane2.getType().getSeats());
+    }
 }
